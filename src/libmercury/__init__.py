155c995367a9d697f97af0bd423b1f8112bca78f
Ahsan Ahmed <ahsan.ahmed3246@gmail.com>
2024-08-09 16:05:52 -0700

    Added nested json objects to validators, as well as refactored the system

--- src/libmercury/__init__.py
+++ src/libmercury/__init__.py
@@ -2,4 +2,5 @@ from werkzeug import Request
 from werkzeug import Response
 from werkzeug.utils import redirect
 from .route_management import *
-from .Validator import *
+from .validation import Validator
+from .validation.validate import validate
